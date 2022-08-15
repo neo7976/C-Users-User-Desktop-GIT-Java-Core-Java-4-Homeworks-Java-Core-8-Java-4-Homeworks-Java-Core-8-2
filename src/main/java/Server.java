07bc4820d641f6c660d58d32eed0be4166edbf5d
Dmitrii Sobin <neo7976@yandex.ru>
2022-08-15 15:08:34 +0300

    Написание кода сообщений Server.java

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
     public static void main(String[] args) {
@@ -17,11 +18,22 @@ public class Server {
                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
                 System.out.println("New connection accept! Port: " + clientSocket.getPort());
+
+
+                out.println("Напиши, как тебя зовут?");
                 final String name = in.readLine();
-                out.println(String.format("Привет %s!, твой порт подключения: [%d ]", name, clientSocket.getPort()));
+                out.println("Ты ребёнок? (Да/Нет");
+                final String ageUser = in.readLine();
+
+                if (ageUser.equals("Да"))
+                    out.printf(String.format("Добро пожаловать в детскую зону развлечений, %s !\n" +
+                            "Давай играть !", name));
+                else
+                    out.printf(String.format("Добро пожаловать во взрослую зону развлечений, %s !\n" +
+                            "Хорошего отдыха или хорошего рабочего дня !", name));
+                }
+            } catch(IOException e){
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
-}
