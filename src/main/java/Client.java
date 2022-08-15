9569b85641180c9cdc415a9ca37e9ac29dc5c630
Dmitrii Sobin <neo7976@yandex.ru>
2022-08-15 14:44:42 +0300

    String host = "netology.homework";

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -6,7 +6,7 @@ import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) {
-        String host = "127.0.0.1";
+        String host = "netology.homework";
         int port = 2125;
 
         try (Socket client = new Socket(host, port);
